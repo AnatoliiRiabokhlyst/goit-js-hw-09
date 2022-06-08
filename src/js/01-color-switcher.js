34e66df7771d56973d022e4ecba5fbea1c368d12
Anatolii <anatoliirst@gmail.com>
2022-06-08 21:36:34 +0200

    add task 1-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+// -----------------------------EXPORT---------------------------------------
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+// -----------------------------FUNCTIONS---------------------------------------
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+    addColor = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function stopChangeColor() {
+    clearInterval(addColor);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+// -----------------------------EVENTS---------------------------------------
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChangeColor);
