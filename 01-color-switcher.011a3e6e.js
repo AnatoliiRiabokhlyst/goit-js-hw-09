88c3fa459f52bfad73a7e429e074582508a6a620
AnatoliiRiabokhlyst <95037551+AnatoliiRiabokhlyst@users.noreply.github.com>
2022-06-09 09:13:08 +0000

    Deploying to gh-pages from @ AnatoliiRiabokhlyst/goit-js-hw-09@8594667dacb53f89d36fdf5850fcf2585ccb04bf 🚀

--- 01-color-switcher.011a3e6e.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){addColor=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(addColor),e.disabled=!1,d.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.011a3e6e.js.map
